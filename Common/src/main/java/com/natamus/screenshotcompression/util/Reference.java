b48a501c58030dfb16eee9bd5b0a42866c584e1b
Rick South <rick@natamus.com>
2026-04-10 02:44:55 +0200

    Screenshot Compression source code for 26.1.2.

---
+++ Common/src/main/java/com/natamus/screenshotcompression/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.screenshotcompression.util;
+
+public class Reference {
+	public static final String MOD_ID = "screenshotcompression";
+	public static final String NAME = "Screenshot Compression";
+	public static final String VERSION = "1.2";
+	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+}
